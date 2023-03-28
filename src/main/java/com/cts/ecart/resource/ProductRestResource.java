2db94488881b08cbedee31756126090312547c66
Praveen Reddy <praveen@Praveens-MacBook-Pro.local>
2023-03-29 02:44:42 +0530

    rest-app-v39

--- src/main/java/com/cts/ecart/resource/ProductRestResource.java
+++ src/main/java/com/cts/ecart/resource/ProductRestResource.java
@@ -12,7 +12,7 @@ public class ProductRestResource {
 
     @GetMapping
     public String f1(){
-        return "Welcome to my APP ......";
+        return "Welcome to my APP .........";
 
     }
 }
