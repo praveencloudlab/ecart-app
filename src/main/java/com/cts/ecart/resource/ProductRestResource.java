d542bf831befa2fbbfe0384227dfe72ed1ede970
Praveen Reddy <praveen@Praveens-MacBook-Pro.local>
2023-03-29 01:22:24 +0530

    rest-app

---
+++ src/main/java/com/cts/ecart/resource/ProductRestResource.java
@@ -0,0 +1,17 @@
+package com.cts.ecart.resource;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/products")
+@CrossOrigin
+public class ProductRestResource {
+
+    @GetMapping
+    public String f1(){
+        return "Welcome to my APP";
+    }
+}
